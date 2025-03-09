63e7f828faf96407bd53a7c893db8bde41bf472e
MuhammadAsad-cmd <ma6530028@gmail.com>
2025-03-10 00:37:19 +0500

    map

--- components/MapComponent/MapComponent.tsx
+++ components/MapComponent/MapComponent.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 
 interface MapComponentProps {
@@ -8,40 +8,41 @@ interface MapComponentProps {
 }
 
 const MapComponent: React.FC<MapComponentProps> = ({ center, path }) => {
-  const [mapLoaded, setMapLoaded] = useState(false);
-  const [loadError, setLoadError] = useState(false);
+  const [googleLoaded, setGoogleLoaded] = useState(false);
 
-  if (loadError) {
-    return (
-      <div className="text-red-500">
-        Error loading Google Maps. Please check your API key, DNS, or network
-        connection.
-      </div>
-    );
-  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (
+        window.google &&
+        window.google.maps &&
+        window.google.maps.SymbolPath
+      ) {
+        setGoogleLoaded(true);
+        clearInterval(interval);
+      }
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <APIProvider
-      apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}
-      onLoad={() => setMapLoaded(true)}
-      onError={() => setLoadError(true)}
-    >
+    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
       <div style={{ height: "400px", width: "100%", borderRadius: "8px" }}>
         <Map
           defaultCenter={center}
           defaultZoom={15}
           mapId={process.env.NEXT_PUBLIC_GOOGLE_MAPS_ID}
         >
-          {mapLoaded && (
+          {googleLoaded && (
             <Marker
               position={center}
               title="Scooter Location"
               icon={{
-                path: google.maps.SymbolPath.CIRCLE,
-                scale: 8,
-                fillColor: "#4285F4",
-                fillOpacity: 1,
-                strokeWeight: 2,
+                path: window.google.maps.SymbolPath.CIRCLE, // Ensures API is loaded
+                scale: 12,
+                fillColor: "#99CA3C",
+                fillOpacity: 6,
+                strokeWeight: 1,
               }}
             />
           )}
